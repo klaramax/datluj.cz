076b868d447884fd7a8ecb72f1a6a0f07565358a
Klara Maxova <klara.maxova@centrum.cz>
2024-05-28 23:13:27 +0200

    Add keyUp event listener, update lettersLeft state

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 interface IWordboxProp {
   word: string;
 }
 
-const Wordbox : React.FC<IWordboxProp> = ({ word }) => {
-  const [lettersLeft] = useState<string>(word);  
+const Wordbox: React.FC<IWordboxProp> = ({ word }) => {
+  const [lettersLeft, setLettersLeft] = useState<string>(word);
   
+  useEffect(() => {
+    const handleKeyUp = (event: KeyboardEvent) => {
+      setLettersLeft((prevLettersLeft) => {
+        if (prevLettersLeft && event.key === prevLettersLeft[0]) {
+          return prevLettersLeft.slice(1);
+        }
+        return prevLettersLeft;
+      });
+    };
+
+    document.addEventListener('keyup', handleKeyUp);
+    
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
   return (
     <div className="wordbox">{lettersLeft}</div>
   );
-};
+}
 
 export default Wordbox;
