533b7d247241e665c60a5a84b3f9d08cc835a69e
Klara Maxova <klara.maxova@centrum.cz>
2024-05-31 19:48:49 +0200

    Add CSS styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Stage from './components/Stage';
+import Keyboard from './components/Keyboard';
+
 
 const App: React.FC = () => {
   return (
     <div className="container">
       <h1>Datlování</h1>
       <Stage />
+      <Keyboard/>
     </div>
   );
 };
