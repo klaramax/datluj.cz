d48273012af249d0ced711394476c56056d40b1a
Klara Maxova <klara.maxova@centrum.cz>
2024-06-01 22:56:19 +0200

    add background image, improve CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from "react";
 
 
 const App: React.FC = () => {
-  return (
-    <div className="container">
-      <h1>Datlování</h1>
-      <Stage />
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="header-wrapper">
+                <h1>Datlování</h1>
+            </div>
+            <Stage/>
+        </div>
+    );
 };
 
 export default App;
