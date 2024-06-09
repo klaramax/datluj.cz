e7c01c168790bb05ba64918ca01a8629a3104f80
Klara Maxova <klara.maxova@centrum.cz>
2024-06-09 17:28:47 +0200

    Format Wordbox component, add event listener for space key to prevent page from default action

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -1,93 +1,103 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 interface IWordboxProp {
-    word: string;
-    active: boolean;
-    onFinish: () => void;
-    onMistake: () => void;
-    onCorrectKeystroke: () => void;
+  word: string;
+  active: boolean;
+  onFinish: () => void;
+  onMistake: () => void;
+  onCorrectKeystroke: () => void;
 }
 
 const Wordbox: React.FC<IWordboxProp> = ({
-                                             word,
-                                             active,
-                                             onFinish,
-                                             onMistake,
-                                             onCorrectKeystroke,
-                                         }) => {
-    // State for remaining letters of the word
-    const [lettersLeft, setLettersLeft] = useState<string>(word);
+  word,
+  active,
+  onFinish,
+  onMistake,
+  onCorrectKeystroke,
+}) => {
+  // State for remaining letters of the word
+  const [lettersLeft, setLettersLeft] = useState<string>(word);
 
-    // State to indicate if the user made a mistake
-    const [mistake, setMistake] = useState<boolean>(false);
+  // State to indicate if the user made a mistake
+  const [mistake, setMistake] = useState<boolean>(false);
 
-    // State to track when onFinish should be called
-    const [shouldFinish, setShouldFinish] = useState<boolean>(false);
+  // State to track when onFinish should be called
+  const [shouldFinish, setShouldFinish] = useState<boolean>(false);
 
-    // State to track when onMistake should be called
-    const [shouldReportMistake, setShouldReportMistake] = useState<boolean>(false);
+  // State to track when onMistake should be called
+  const [shouldReportMistake, setShouldReportMistake] =
+    useState<boolean>(false);
 
-    // State to track when onCorrectKeystroke should be called
-    const [shouldReportCorrectKeystroke, setShouldReportCorrectKeystroke] = useState<boolean>(false);
+  // State to track when onCorrectKeystroke should be called
+  const [shouldReportCorrectKeystroke, setShouldReportCorrectKeystroke] =
+    useState<boolean>(false);
 
-    useEffect(() => {
-        if (shouldFinish) {
-            onFinish();
+  useEffect(() => {
+    if (shouldFinish) {
+      onFinish();
+    }
+  }, [shouldFinish, onFinish]);
+
+  useEffect(() => {
+    if (shouldReportMistake) {
+      onMistake();
+      setShouldReportMistake(false);
+    }
+  }, [shouldReportMistake, onMistake]);
+
+  useEffect(() => {
+    if (shouldReportCorrectKeystroke) {
+      onCorrectKeystroke();
+      setShouldReportCorrectKeystroke(false);
+    }
+  }, [shouldReportCorrectKeystroke, onCorrectKeystroke]);
+
+  // useEffect to add and remove keyup event listener on active word
+  useEffect(() => {
+    if (!active) return;
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      setLettersLeft((prevLettersLeft) => {
+        if (prevLettersLeft) {
+          if (event.key === prevLettersLeft[0]) {
+            setMistake(false);
+            const newLettersLeft = prevLettersLeft.slice(1);
+            if (newLettersLeft === '') {
+              setShouldFinish(true);
+            }
+            setShouldReportCorrectKeystroke(true);
+            return newLettersLeft;
+          } else {
+            setMistake(true);
+            setShouldReportMistake(true);
+          }
         }
-    }, [shouldFinish, onFinish]);
+        return prevLettersLeft;
+      });
+    };
 
-    useEffect(() => {
-        if (shouldReportMistake) {
-            onMistake();
-            setShouldReportMistake(false);
-        }
-    }, [shouldReportMistake, onMistake]);
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === ' ') {
+        event.preventDefault(); // Prevent default action for space key
+      }
+    };
 
-    useEffect(() => {
-        if (shouldReportCorrectKeystroke) {
-            onCorrectKeystroke();
-            setShouldReportCorrectKeystroke(false);
-        }
-    }, [shouldReportCorrectKeystroke, onCorrectKeystroke]);
+    document.addEventListener('keyup', handleKeyUp);
+    document.addEventListener('keydown', handleKeyDown);
 
-    // useEffect to add and remove keyup event listener on active word
-    useEffect(() => {
-        if (!active) return;
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [active, onFinish, onMistake, onCorrectKeystroke]);
 
-        const handleKeyUp = (event: KeyboardEvent) => {
-            setLettersLeft((prevLettersLeft) => {
-                if (prevLettersLeft) {
-                    if (event.key === prevLettersLeft[0]) {
-                        setMistake(false);
-                        const newLettersLeft = prevLettersLeft.slice(1);
-                        if (newLettersLeft === "") {
-                            setShouldFinish(true);
-                        }
-                        setShouldReportCorrectKeystroke(true);
-                        return newLettersLeft;
-                    } else {
-                        setMistake(true);
-                        setShouldReportMistake(true);
-                    }
-                }
-                return prevLettersLeft;
-            });
-        };
-
-        document.addEventListener('keyup', handleKeyUp);
-
-        return () => {
-            document.removeEventListener('keyup', handleKeyUp);
-        };
-    }, [active, onFinish, onMistake, onCorrectKeystroke]);
-
-    return (
-        // Render the div with a dynamic class based on the mistake state
-        <div className={`wordbox ${mistake ? 'wordbox--mistake' : ''}`}>
-            {lettersLeft}
-        </div>
-    );
-}
+  return (
+    // Render the div with a dynamic class based on the mistake state
+    <div className={`wordbox ${mistake ? 'wordbox--mistake' : ''}`}>
+      {lettersLeft}
+    </div>
+  );
+};
 
 export default Wordbox;
